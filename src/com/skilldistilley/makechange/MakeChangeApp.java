d5a873b9d845da9f076d3ef19034a7c4c25f416e
Cody Fulker <codyfulker@SDMacBookPro3-2.local>
2019-07-12 17:38:06 -0600

    How much/given?

--- src/com/skilldistilley/makechange/MakeChangeApp.java
+++ src/com/skilldistilley/makechange/MakeChangeApp.java
@@ -1,10 +1,31 @@
 package com.skilldistilley.makechange;
 
+import java.util.Scanner;
+
 public class MakeChangeApp {
 
+	static Scanner kb;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		kb = new Scanner(System.in);
+		double itemPrice = 0;
+		double moneyGiven = 0;
+		
+		itemCost();
+		moneyTendered();
+		
+		kb.close();
+	}
+	
+	public static double itemCost() {
+		System.out.print("How much does the item cost? $");
+		double itemPrice = kb.nextDouble();
+		return itemPrice;
+	}
+	public static double moneyTendered() {
+		System.out.print("How much are you paying with? $");
+		double moneyGiven = kb.nextDouble();
+		return moneyGiven;
 	}
 
 }
