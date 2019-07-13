c0e8cb90a208bc59844c779d67f22dab43eef121
Cody Fulker <codyfulker@SDMacBookPro3-2.local>
2019-07-13 12:33:32 -0600

    Added do while

--- src/com/skilldistilley/makechange/MakeChangeApp.java
+++ src/com/skilldistilley/makechange/MakeChangeApp.java
@@ -8,68 +8,79 @@ public class MakeChangeApp {
 
 	public static void main(String[] args) {
 		kb = new Scanner(System.in);
-		double price = 0.00, change = 0.00;
-		double cash = 0.00, change1 = 0.0; 
-		int penny = 0, nickel ;
-		int dime, quarter, dollar;
-		int five, ten, twenty = 0;
-		double pennyV = 0.01, nickelV = 0.05;
-		double dimeV = 0.10, quarterV = 0.25;
+		int price, cash, change;
+//		String choice = "";
+		boolean newTransaction = true;
 
-		System.out.print("How much does the item cost? $");
-		price = kb.nextDouble();
+		do {
+			System.out.print("How much does the item cost? $");
+			price = (int) ((kb.nextDouble() * 100)+0.001);
+			System.out.print("How much are you paying with? $");
+			cash = (int) ((kb.nextDouble() * 100)+0.001);
+			kb.nextLine();
+			
+			if (cash > price) {
+				change = cash - price;
+//				System.out.println(change);
+				double change1 = (double) change;
+				change1 /= 100;
 
-		System.out.print("How much are you paying with? $");
-		cash = kb.nextDouble();
+				System.out.printf("Your change is $%5.2f%n",change1);
 
-		change1 = cash - price;
-		System.out.println("You're change is $"+change);
-		
-		twenty = (int) Math.floor(change - twenty / 20);
-
-	    ten = (int) Math.floor((change - twenty * 20) / 10);
-
-	    five = (int) Math.floor((change - twenty * 20 - ten * 10) / 5);
-
-	    dollar = (int) Math.floor((change -twenty * 20 - ten * 10 - five * 5) / 1);
-
-	    quarter = (int) Math.floor((change -  twenty * 20 - ten * 10 - five * 5 - dollar * 1)
-	            / quarterV);
-
-	    dime = (int) Math.floor((change - twenty * 20 - ten * 10 - five * 5 - dollar * 1 
-	            - quarter * quarterV) / dimeV);
-
-	    nickel = (int) Math.floor((change - twenty * 20 - ten * 10 - five * 5 - dollar * 1 
-	            - quarter * quarterV - dime * dimeV) / nickelV);
-
-	    penny = (int) Math.floor((change - twenty * 20 - ten * 10 - five * 5 - dollar * 1 
-	            - quarter * quarterV - dime * dimeV - nickel * nickelV) / penny);
-	    
-	    
-	    double remainder = change1; //make a copy of change variable so it remains final.
-	    int count=0;
-	    System.out.println("Your change is ");
-	    if(remainder>=20){
-	        twenty = (int)remainder/20;
-	        remainder %= 20;
-	        System.out.print(twenty+ " twenties, ");
-	    }
-	    if(remainder>=10){
-	        ten = (int)remainder/10;
-	        remainder %= 10;
-	        System.out.println(ten+" tens, ");
-	    }
-	    if(remainder>=5){
-	        five = (int)remainder/5;
-	        remainder %= 5;
-	        System.out.println(five+" fives, ");
-	    }
-	    if(remainder>=1) {
-	    	dollar = (int)remainder/1;
-	    		remainder %= 1;
-	    		System.out.println(dollar+" ones, ");
-	    }
-		
+				int twenties = change / 2000;
+				if (twenties > 0) {
+					change %= 2000;
+					System.out.println(twenties + " $20 bill(s)");
+				}
+				int tens = change / 1000;
+				if (tens > 0) {
+					change %= 1000;
+					System.out.println(tens + " $10 bill(s)");
+				}
+				int fives = change / 500;
+				if (fives > 0) {
+					change %= 500;
+					System.out.println(fives + " $5 bill(s)");
+				}
+				int ones = change / 100;
+				if (ones > 0) {
+					change %= 100;
+					System.out.println(ones + " $1 bill(s)");
+				}
+				int quarters = change / 25;
+				if (quarters > 0) {
+					change %= 25;
+					System.out.println(quarters + " quarter(s)");
+				}
+				int dimes = change / 10;
+				if (dimes > 0) {
+					change %= 10;
+					System.out.println(dimes + " dime(s)");
+				}
+				int nickels = change / 5;
+				if (nickels > 0) {
+					change %= 5;
+					System.out.println(nickels + " nickel(s)");
+				}
+				int pennies = change;
+				System.out.println(pennies + " penny coin(s)");
+			}
+			if (cash < price) {
+				System.out.println("Not enough money!");
+			} else if (cash == price) {
+				System.out.println("No change is necessary!");
+			}
+			System.out.println("Do you want to go again (Y/N)?" );
+			String continueLoopString = kb.nextLine();
+			if (continueLoopString.equalsIgnoreCase("Y")) {
+				newTransaction = true;
+			}
+			else if (continueLoopString.equalsIgnoreCase("N")) {
+				newTransaction = false;
+				kb.nextLine();
+			}
+			
+		} while (newTransaction);
+		kb.close();
 	}
-
 }
